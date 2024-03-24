0df6a4427d5b0a5609abe5e1f0bd12c288ccb402
alexrg00 <grabikalex@gmail.com>
2024-03-24 13:53:15 -0400

    refactored map data to be contained in an object

--- script.js
+++ script.js
@@ -7,7 +7,24 @@ const gridCellScaler = rootStyles.getPropertyValue('--grid-cell-scaler');
 const mapWidth = rootStyles.getPropertyValue('--min-map-width-ratio');
 const mapHeight = rootStyles.getPropertyValue('--min-map-height-ratio');
 
-
+// TODO: This is the basic idea, to have everything about each map in objects that can be reused
+// take a look at this then process, this is not commited, chekc the last commit to see where you are starting from.
+// the main point is to make each map have an object
+const gameMaps = {
+   lobby: {
+      startingPoint: {x: 0, y: 0},
+      url: "./assets/UqMTVX.gif",
+      imageObject: null,
+      collisionList: [
+         {
+            left: 79, 
+            top: 100, 
+            width: 159,
+            height: 140
+         }
+      ]
+   }
+}
 
 /** 
  * Direction key state 
@@ -43,15 +60,14 @@ async function getImageFromSource(src) {
 const speed = 1; 
 
 async function main() {
-   /**
-    * Get map image source width and height
-    */
-   const computedStyle = window.getComputedStyle(map);
-   const backgroundImage = computedStyle.getPropertyValue('background-image');
-   console.log(backgroundImage)
-   // remove 'url()' from the image src string
-   const imageUrl = backgroundImage.match(/url\("([^"]+)"\)/)[1];
-   mapSource = await getImageFromSource(imageUrl)
+   //assign game map image objects via url
+   gameMaps.lobby.imageObject = await getImageFromSource(gameMaps.lobby.url);
+   // gameMaps...imageObject = await getImageFromSource(...)
+   //...
+
+
+   mapSource = gameMaps.lobby.imageObject
+   currentMap = gameMaps.lobby
 
    console.log(mapSource.width, mapSource.height)
 
@@ -65,8 +81,8 @@ async function main() {
    // TODO: future positions will need to account for the position of the player sprite adjustment. play position is not the top left of sprite, but where it's standing
    let char_coords = {
       // coordinates from the original * map_ratio[]
-      'x_axis': (mapSource.width) * map_ratios[X_AXIS],
-      'y_axis': (mapSource.height) * map_ratios[Y_AXIS]
+      'x_axis': gameMaps.lobby.startingPoint.x,
+      'y_axis': gameMaps.lobby.startingPoint.y
    }
 
    // State of which arrow keys we are holding down
@@ -87,20 +103,6 @@ async function main() {
       }
    });
 
-   // collision detection:
-   let impassableTerrains = [{
-         left: 79 * map_ratios[X_AXIS], 
-         top: 100 * map_ratios[Y_AXIS], 
-         width: 159  * map_ratios[X_AXIS],
-         height: 140 * map_ratios[Y_AXIS] 
-      }
-   ];
-
-   // Define impassable terrains using (left, top, width, height) coordinates
-   // TODO: the 1026/ 316 and 1154/364 is what is rendered on the browser css
-   // this is only to display the collisions, but not create them  
-   // TODO: make the pixel conversion automatic in the game loop even if the size of the map changes
-
    // collision visuals:
    let showCollisions = false;
 
@@ -114,18 +116,9 @@ async function main() {
    //Set up the game loop
    const step = () => {
       // this needs to be updated every frame because the browser window may be resized by the user 
-      let impassableTerrainsVisual = [{
-         // getting map width/height to convert coordinates from original to browser render
-            left: 79 * map.offsetWidth / mapSource.width, 
-            top: 100 * map.offsetHeight / mapSource.height, 
-            width: 159  * map.offsetWidth / mapSource.width,
-            height: 140 * map.offsetHeight / mapSource.height
-         }
-      ];
-
-      drawImpassableTerrains(showCollisions, map, impassableTerrainsVisual);
+      drawImpassableTerrains(showCollisions, map, gameMaps.lobby.collisionList, mapSource);
       
-      placeCharacter(held_directions, directions, char_coords, X_AXIS, speed, Y_AXIS, character, map, impassableTerrains);
+      placeCharacter(held_directions, directions, char_coords, X_AXIS, speed, Y_AXIS, character, map, gameMaps.lobby.collisionList, map_ratios);
       window.requestAnimationFrame(() => {
          // call itself every frame
          step();
@@ -187,7 +180,7 @@ async function main() {
 
 main()
 
-function placeCharacter(held_directions, directions, char_coords, x_axis, speed, y_axis, character, map, impassableTerrains) {
+function placeCharacter(held_directions, directions, char_coords, x_axis, speed, y_axis, character, map, impassableTerrains, map_ratios) {
    let pixelSize = parseInt(
       getComputedStyle(document.documentElement).getPropertyValue('--pixel-size')
    );
@@ -212,7 +205,7 @@ function placeCharacter(held_directions, directions, char_coords, x_axis, speed,
 
    // use this to get the size of the map for the player collisions
    console.log(`${char_coords[x_axis]}, ${char_coords[y_axis]}`)
-   let collision = checkCollision(char_coords[x_axis], char_coords[y_axis], impassableTerrains);
+   let collision = checkCollision(char_coords[x_axis], char_coords[y_axis], impassableTerrains, map_ratios);
 
    if (collision) {
       console.log('collision detected')
@@ -224,7 +217,7 @@ function placeCharacter(held_directions, directions, char_coords, x_axis, speed,
    } 
 }
 
-function drawImpassableTerrains(showCollisions, map, impassableTerrains) {
+function drawImpassableTerrains(showCollisions, map, impassableTerrains, mapSource) {
    let canvas = document.getElementById("terrainCanvas");
    let ctx = canvas.getContext("2d");
 
@@ -236,25 +229,32 @@ function drawImpassableTerrains(showCollisions, map, impassableTerrains) {
    canvas.width = width;
    canvas.height = height;
 
-   // Clear canvas
-   //ctx.clearRect(0, 0, canvas.width, canvas.height); 
+   let widthRatio = map.offsetWidth / mapSource.width;
+   let heightRatio = map.offsetHeight / mapSource.height;
 
    if (showCollisions) {
       ctx.fillStyle = "rgba(255, 0, 0, 0.5)"; // Color for impassable terrains
       for (let i = 0; i < impassableTerrains.length; i++) {
          let terrain = impassableTerrains[i];
-         ctx.fillRect(terrain.left, terrain.top, terrain.width, terrain.height);
+         ctx.fillRect(
+            terrain.left * widthRatio, 
+            terrain.top * heightRatio, 
+            terrain.width * widthRatio, 
+            terrain.height * heightRatio
+         );
       }
    }
 }
 
 
 // Check if character intersects with any impassable terrain
-function checkCollision(x_char, y_char, impassableTerrains) {
+function checkCollision(x_char, y_char, impassableTerrains, map_ratios) {
    for (let i = 0; i < impassableTerrains.length; i++) {
       let terrain = impassableTerrains[i];
-      if (x_char >= terrain.left && x_char <= terrain.left + terrain.width &&
-            y_char >= terrain.top && y_char <= terrain.top + terrain.height) {
+      if (x_char >= terrain.left * map_ratios[X_AXIS] && 
+         x_char <= terrain.left * map_ratios[X_AXIS] + terrain.width * map_ratios[X_AXIS] &&
+            y_char >= terrain.top * map_ratios[Y_AXIS] && 
+            y_char <= terrain.top * map_ratios[Y_AXIS] + terrain.height * map_ratios[Y_AXIS]) {
             return true;  // Collision detected
       }
    }
